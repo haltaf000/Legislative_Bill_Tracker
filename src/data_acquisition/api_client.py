ed5f2e59d8984137c9256a0306606ce7ffe521b7
Your Name <you@example.com>
2023-11-16 14:27:01 -0500

    fixed app.py

--- src/data_acquisition/api_client.py
+++ src/data_acquisition/api_client.py
@@ -1,35 +1,25 @@
 import requests
 
-# Replace 'API_KEY' and 'API_ENDPOINT' with your actual key and endpoint URLs
-API_KEY = 'your_api_key_here'
+API_KEY = 'your_api_key_here' # visiting user please go get individual api's for usage to do important data privacy!
 NY_ASSEMBLY_ENDPOINT = 'https://api.assembly.ny.gov/bills'
 NY_SENATE_ENDPOINT = 'https://api.senate.ny.gov/bills'
 
-def get_assembly_bills(session, params=None):
-    """
-    Fetches bill data from the New York Assembly API.
-    """
+def get_bills(endpoint, session, params=None):
     headers = {'Authorization': f'Bearer {API_KEY}'}
-    response = session.get(NY_ASSEMBLY_ENDPOINT, headers=headers, params=params)
-    response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
-    return response.json()
-
-def get_senate_bills(session, params=None):
-    """
-    Fetches bill data from the New York Senate API.
-    """
-    headers = {'Authorization': f'Bearer {API_KEY}'}
-    response = session.get(NY_SENATE_ENDPOINT, headers=headers, params=params)
+    response = session.get(endpoint, headers=headers, params=params)
     response.raise_for_status()
     return response.json()
 
 def main():
-    # Use a session for connection pooling and reusing the TCP connection
     with requests.Session() as session:
-        # Example usage: Fetching bills from the Assembly API
-        assembly_bills = get_assembly_bills(session, params={'type': 'passed'})
+        assembly_bills = get_bills(NY_ASSEMBLY_ENDPOINT, session, params={'type': 'passed'})
         print("Assembly Bills:", assembly_bills)
 
-        # Example usage: Fetching bills from the Senate API
-        senate_bills = get
+        senate_bills = get_bills(NY_SENATE_ENDPOINT, session, params={'type': 'passed'})
+        print("Senate Bills:", senate_bills)
+
+if __name__ == "__main__":
+    main()
+
+     
 
